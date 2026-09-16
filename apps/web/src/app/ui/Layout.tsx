2483e92bcb96375d6d9a661872b0e917d850ba5e
devcdd <developer.cdd@gmail.com>
2026-09-16 11:16:17 +0900

    fix: 모바일에서 페이지 폭이 연도 칩 전체 폭으로 늘어나 줌아웃되는 문제 수정

--- apps/web/src/app/ui/Layout.tsx
+++ apps/web/src/app/ui/Layout.tsx
@@ -3,13 +3,17 @@ import { AppHeader } from '@/widgets/app-header'
 import { AppFooter } from '@/widgets/app-footer'
 import { ScrollToTop } from '@/app/ui/ScrollToTop'
 import { ScrollTopButton } from '@/shared/ui/ScrollTopButton'
+import styles from './Layout.module.css'
 
 export function Layout() {
   return (
     <>
       <AppHeader />
       <ScrollToTop />
-      <Outlet />
+      {/* flex 컬럼 안에서 margin:auto 페이지 컨테이너가 stretch를 잃고 max-content 폭으로 튀지 않도록 block main으로 감쌈 */}
+      <main className={styles.main}>
+        <Outlet />
+      </main>
       <AppFooter />
       <ScrollTopButton />
     </>
